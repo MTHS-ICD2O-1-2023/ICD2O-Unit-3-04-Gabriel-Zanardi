006fd9c704f4858baa541433b2798f5f5dc2c707
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-10 18:38:40 +0000

    did css, js, and index

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function myButtonClicked() {
+ // input
+ const fahrenheitTemp = parseFloat(document.getElementById("fahrenheit-temp").value)
+
+ // process
+ const celsiusTemp = (fahrenheitTemp - 32) * 5.0 / 9.0
+ const finalAnswer = celsiusTemp.toFixed(2)
+
+ // output
+ document.getElementById("fahren-to-celsius").innerHTML =
+   "The temperature in Celsius is " + finalAnswer + "C°."
+}
